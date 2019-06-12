29f1b3a11880940be04d3d103b42a449061ea02b
naga oleti <venu.babu80@gmail.com>
2019-06-12 07:53:04 +0400

    commit to git

---
+++ src/components/counter.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    //value: this.props.counter.value,
+    tags: ["tag1", "tag2", "tag3"],
+    imageUrl: "https://picsum.photos/200"
+  };
+
+  styles = {
+    fontSize: 15,
+    fontWeight: "bold"
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+      // ajax call and get new data from server
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Counter - UNmount");
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  // constructor() {
+  //   super();
+  //   //console.log("constructor", this);
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  render() {
+    console.log("counter - rendered");
+    return (
+      <div className="row">
+        <div className="col-1">
+          <span style={this.styles} className={this.getBadgeClasses()}>
+            {this.formatCount()}
+          </span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? true : false}
+          >
+            -
+          </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm m-2"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
